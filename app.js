50c4c80f340b0327a63fbd937063f89bb6e3a0ca
richardswei <wei.richard.s@gmail.com>
2021-06-20 21:32:00 -0700

    update the shoot controller and data access service to successfully post and get shoots

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dataAccessService = require('./data/data_access_service');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1', apiV1Router);
+
+// initialize data persistence
+dataAccessService.initializeStorage();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/shootController.js
+++ controllers/shootController.js
@@ -1,16 +1,17 @@
 var Shoot = require('../models/shoot');
+var dataAccessService = require('../data/data_access_service')
 
 // Display list of all Shoots.
-exports.shoot_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Shoot list');
+exports.shoot_list = async function(req, res) {
+    res.send(await dataAccessService.get_all_shoots());
 };
 
 // Display detail page for a specific Shoot.
-exports.shoot_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Shoot detail: ' + req.params.id);
+exports.shoot_detail = async function(req, res) {
+    res.send(await dataAccessService.get_shoot_by_shootId(req.params.shootId));
 };
 
 // Handle Shoot create on POST.
-exports.shoot_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Shoot create POST');
+exports.shoot_create_post = async function(req, res) {
+    res.send(await dataAccessService.post_shoot(req.body));
 };
